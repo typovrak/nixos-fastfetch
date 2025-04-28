66335851e8c6afd8d9738894eaf6468304724243
typovrak <typovrak@gmail.com>
2025-04-28 20:49:29 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,18 @@
 { config, pkgs, ... }:
 
 let
+	username = config.username;
 	group = config.users.users.${config.username}.group or "users";
 	home = config.users.users.${config.username}.home;
 in {
 	system.activationScripts.fastfetch = ''
 		mkdir -p ${home}/.config
-		chown ${config.username}:${group} ${home}/.config
+		chown ${username}:${group} ${home}/.config
 		chmod 700 ${home}/.config
 
 		rm -rf ${home}/.config/fastfetch
 		mkdir ${home}/.config/fastfetch
-		chown ${config.username}:${group} ${home}/.config/fastfetch
+		chown ${username}:${group} ${home}/.config/fastfetch
 		chmod 700 ${home}/.config/fastfetch
 
 		cp ${./config.jsonc} ${home}/.config/fastfetch/config.jsonc
